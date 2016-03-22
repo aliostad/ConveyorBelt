7383b2965f3ed964c378b4498994a8653dbcca5d
Ali Kheyrollahi <aliostad@gmail.com>
2016-03-22 22:07:36 +0000

    retrying if unsuccessful comes

--- src/ConveyorBelt.Tooling/ElasticserachBatchPusher.cs
+++ src/ConveyorBelt.Tooling/ElasticserachBatchPusher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -16,10 +17,9 @@ namespace ConveyorBelt.Tooling
     public class ElasticsearchBatchPusher : IElasticsearchBatchPusher
     {
         private string _esUrl;
-        private StringBuilder _stringBuilder = new StringBuilder();
         private int _batchSize;
-        private int _numberOfRecords = 0;
         private IHttpClient _httpClient;
+        private Batch _batch = new Batch();
 
         // no reason for thread sync/concurrent since this will be called only by a single thread
         private Dictionary<string, SimpleFilter> _filters = new Dictionary<string, SimpleFilter>();
@@ -34,19 +34,40 @@ namespace ConveyorBelt.Tooling
         private async Task PushbatchAsync()
         {
 
-            if (_stringBuilder.Length == 0)
+            if (_batch.Count == 0)
                 return;
 
             try
             {
-                var responseMessage = await  _httpClient.PostAsync(_esUrl + "_bulk",
-                    new StringContent(_stringBuilder.ToString(),
+                int retry = 0;
+                List<bool> statuses = null;
+                do
+                {
+                    var responseMessage = await  _httpClient.PostAsync(_esUrl + "_bulk",
+                    new StringContent(_batch.ToString(),
                         Encoding.UTF8, "application/json"));
-                _stringBuilder.Clear();
-                _numberOfRecords = 0;
-                responseMessage.EnsureSuccessStatusCode();
+                    var content = responseMessage.Content == null ? "" :
+                        (await responseMessage.Content.ReadAsStringAsync());
 
-                TheTrace.TraceInformation("ConveyorBelt_Pusher: Pushing to {0}", _esUrl);
+                    if(!responseMessage.IsSuccessStatusCode)
+                        throw new ApplicationException(string.Format("Unsuccessful ES bulk: {0} - {1}", responseMessage.StatusCode, content));
+
+                    dynamic j = JObject.Parse(content);
+                    if (j == null || j.items == null)
+                        throw new ApplicationException(string.Format("Unsuccessful ES bulk - items null: {0}", content));
+
+                    var items = (JArray) j.items;
+                    statuses = items.Children<JObject>().Select(x => x.Properties().First().Value["status"].Value<int>())
+                        .Select(y => y >= 200 && y <= 299).ToList();
+
+                    TheTrace.TraceInformation("ConveyorBelt_Pusher: Pushing to {0}", _esUrl);
+                } while (_batch.Prune(statuses) > 0 && retry++ < 3);
+
+                if (_batch.Count > 0)
+                {
+                    TheTrace.TraceWarning("WARNING!!! Some residual documents could not be inserted even after retries: {0}", _batch.Count);
+                    _batch.Clear();
+                }
             }
             catch (Exception e)
             {
@@ -94,18 +115,14 @@ namespace ConveyorBelt.Tooling
                     doc[keyValue.Key] = keyValue.Value;
                 }
             }
-                
-            _stringBuilder.Append(JsonConvert.SerializeObject(op).Replace("\r\n", " "));
-            _stringBuilder.Append('\n');
-            _stringBuilder.Append(doc.ToString().Replace("\r\n", " "));
-            _stringBuilder.Append('\n');
 
-            if ((_numberOfRecords++) >= _batchSize)
+            _batch.AddDoc(JsonConvert.SerializeObject(op).Replace("\r\n", " "), doc.ToString().Replace("\r\n", " "));
+
+            if (_batch.Count >= _batchSize)
             {
-                var nrec = _numberOfRecords;
                 await PushbatchAsync();
                 TheTrace.TraceInformation("ConveyorBelt_Pusher: Pushed {0} records to ElasticSearch for {1}-{2}",
-                    nrec,
+                    _batch.Count,
                     source.PartitionKey,
                     source.RowKey);
             }
@@ -125,6 +142,56 @@ namespace ConveyorBelt.Tooling
         {
             return PushbatchAsync();
         }
-    }
 
+        
+
+        internal class Batch
+        {
+             private List<Tuple<string, string>> _list = new List<Tuple<string, string>>();
+
+            public void AddDoc(string op, string doc)
+            {
+                _list.Add(new Tuple<string, string>(op, doc));
+            }
+
+            public int Count
+            {
+                get { return _list.Count; }
+            }
+
+            public void Clear()
+            {
+                _list.Clear();
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                foreach (var item in _list)
+                {
+                    sb.Append(item.Item1);
+                    sb.Append('\n');
+                    sb.Append(item.Item2);
+                    sb.Append('\n');
+                }
+
+                return sb.ToString();
+            }
+
+            public int Prune(IList<bool> statuses)
+            {
+                if(statuses.Count != _list.Count)
+                    throw new InvalidOperationException(string.Format(
+                        "Statuses should have exactly the same number of items. {0} vs. statuses {1}", _list.Count, statuses.Count));
+
+                for (var i = _list.Count-1; i >= 0; i--) // start from the end
+                {
+                    if(statuses[i])
+                        _list.RemoveAt(i);
+                }
+
+                return _list.Count;
+            }
+        }
+    }
 }
--- test/ConveyorBelt.Tooling.Test/ELasticBatchPusherTests.cs
+++ test/ConveyorBelt.Tooling.Test/ELasticBatchPusherTests.cs
@@ -1,17 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
 using Moq;
 using Xunit;
 
 namespace ConveyorBelt.Tooling.Test
 {
-    public class ELasticBatchPusherTests
+    public class ElasticBatchPusherTests
     {
-       
+        [Fact]
+        public void TriesMultipleTimes()
+        {
+            var client = new Mock<IHttpClient>(MockBehavior.Loose);
+            var pusher = new ElasticsearchBatchPusher(client.Object, "http://google.com");
+            var summary = new DiagnosticsSourceSummary()
+            {
+                ConnectionString = String.Empty,
+                PartitionKey = "pk",
+                RowKey = "rk"
+            };
+            
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            client.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>())).ReturnsAsync(
+                new HttpResponseMessage(HttpStatusCode.Accepted)
+                {
+                    Content = new StringContent(File.ReadAllText(@"data\es_response_sumsuxes.json"))
+                });
 
+            Assert.Throws<AggregateException>(() => pusher.FlushAsync().Wait());
+
+        }
+
+        [Fact]
+        public void TriesMultipleTimesAndSucceed()
+        {
+            var client = new Mock<IHttpClient>(MockBehavior.Loose);
+            var pusher = new ElasticsearchBatchPusher(client.Object, "http://google.com");
+            var summary = new DiagnosticsSourceSummary()
+            {
+                ConnectionString = String.Empty,
+                PartitionKey = "pk",
+                RowKey = "rk"
+            };
+
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            pusher.PushAsync(new DynamicTableEntity("pk", "rk"), summary).Wait();
+            client.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>())).ReturnsAsync(
+                new HttpResponseMessage(HttpStatusCode.Accepted)
+                {
+                    Content = new StringContent(File.ReadAllText(@"data\es_response_allsuxes.json"))
+                });
+
+            pusher.FlushAsync().Wait();
+
+        }
     }
 }
