65ac7f7fad5b8acecb9d77cb0a16802c3d2286ff
sergii-sakharov-asos <sergii.sakharov@asos.com>
2018-03-20 14:15:26 +0000

    adding tests

--- src/ConveyorBelt.Tooling/Parsing/IisLogParser.cs
+++ src/ConveyorBelt.Tooling/Parsing/IisLogParser.cs
@@ -11,10 +11,11 @@ namespace ConveyorBelt.Tooling.Parsing
     {
         public IEnumerable<IDictionary<string, string>> Parse(Stream body, Uri id, DiagnosticsSourceSummary source, long startPosition = 0, long endPosition = 0)
         {
-            if (body.Position != 0
-            ) // this is just to make sure we read the fields. If we start in the middle, we will miss the fields
-                body.Position = 0;
+            return ParseInternal(body, id, source, startPosition, startPosition, endPosition);
+        }
 
+        private IEnumerable<IDictionary<string, string>> ParseInternal(Stream body, Uri id, DiagnosticsSourceSummary source, long startReadPosition, long startParsePosition, long endPosition)
+        {
             if (endPosition == 0)
                 endPosition = long.MaxValue;
 
@@ -22,19 +23,19 @@ namespace ConveyorBelt.Tooling.Parsing
             var partitionKey = string.Join("_", idSegments.Take(idSegments.Length - 1));
             var rowKeyPrefix = Path.GetFileNameWithoutExtension(idSegments.Last());
 
-            var fields = startPosition > 0 ? ReadFirstHeaders(body) : null;
-            body.Seek(startPosition, SeekOrigin.Begin);
+            var fields = startReadPosition > 0 ? ReadFirstHeaders(body) : null;
+            body.Seek(startReadPosition, SeekOrigin.Begin);
 
-            var reTryFromBeginning = false;
+            var headersHaveChanged = false;
             using (var reader = new StreamReader(body, Encoding.ASCII, true, 1024 * 1024 * 5, true))
             {
                 string line;
-                var offset = startPosition;
+                var offset = startReadPosition;
                 var lineCount = 0;
                 while (offset < endPosition && (line = reader.ReadLine()) != null)
                 {
                     lineCount++;
-                    offset += line.Length;
+                    offset += line.Length + Environment.NewLine.Length;
                     if (line.StartsWith("#Fields: "))
                     {
                         fields = BuildFields(line);
@@ -44,75 +45,83 @@ namespace ConveyorBelt.Tooling.Parsing
                     if (line.StartsWith("#"))
                         continue;
 
+                    //skip until start offset in case of re-read
+                    if (offset < startParsePosition)
+                        continue;
+
                     var entries = GetEntries(line);
                     if (fields?.Length + 2 != entries.Length)
                     {
-                        if (startPosition == 0)
+                        if (startReadPosition == 0)
                             throw new InvalidOperationException("fields column mismatch");
 
-                        //e.g. in case startPosition was pointing to the middle of the line
+                        //e.g. in case startReadPosition was pointing to the middle of the line
                         //have to skip to next line
                         if (lineCount == 1)
                             continue;
 
-                        //headers have changed since the beginning of the file - have to read whole file
-                        reTryFromBeginning = true;
+                        headersHaveChanged = true;
                         break;
                     }
 
-                    const int stampEntryCount = 2;
-                    var datetime = string.Join("T", entries.Take(stampEntryCount));
-
-                    var doc = new Dictionary<string, string>(fields.Length + 3)
-                    {
-                        {"@timestamp", datetime},
-                        {"PartitionKey", partitionKey},
-                        {"RowKey", $"{rowKeyPrefix}_{offset}"},
-                        {"cb_type", source.TypeName}
-                    };
-
-                    for (var i = 0; i < fields.Length; i++)
-                    {
-                        var name = fields[i];
-                        var value = entries[stampEntryCount + i];
-
-                        if (value == "-") // to get around the missing fields in IIS... this is how it does its
-                            continue;
-
-                        if (name != DiagnosticsSource.CustomAttributesFieldName)
-                        {
-                            doc[name] = value;
-                        }
-                        else
-                        {
-                            if (string.IsNullOrWhiteSpace(value))
-                                continue;
-
-                            foreach (var pair in value.Split(';'))
-                            {
-                                var parts = pair.Split(new[] {'='}, 2);
-                                if (parts.Length == 2)
-                                    doc[parts[0]] = parts[1];
-                            }
-                        }
-                    }
-
-                    yield return doc;
+                    yield return ParseEntity(fields, entries, source.TypeName, partitionKey, $"{rowKeyPrefix}_{offset}");
                 }
             }
 
-            if (!reTryFromBeginning) yield break;
-            
-            foreach (var doc in Parse(body, id, source, 0, endPosition))
+            if (!headersHaveChanged) yield break;
+
+            //headers have changed since the beginning of the file - have to read whole file
+            foreach (var doc in ParseInternal(body, id, source, 0, startParsePosition, endPosition))
             {
                 yield return doc;
             }
         }
 
+        private static Dictionary<string, string> ParseEntity(string[] fields, string[] entries, string cbType, string partitionKey, string rowKey)
+        {
+            const int stampEntryCount = 2;
+            var datetime = string.Join("T", entries.Take(stampEntryCount));
+
+            var doc = new Dictionary<string, string>(fields.Length + 3)
+            {
+                {"@timestamp", datetime},
+                {"PartitionKey", partitionKey},
+                {"RowKey", rowKey},
+                {"cb_type", cbType}
+            };
+
+            for (var i = 0; i < fields.Length; i++)
+            {
+                var name = fields[i];
+                var value = entries[stampEntryCount + i];
+
+                if (value == "-") // to get around the missing fields in IIS... this is how it does its
+                    continue;
+
+                if (name != DiagnosticsSource.CustomAttributesFieldName)
+                {
+                    doc[name] = value;
+                }
+                else
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                        continue;
+
+                    foreach (var pair in value.Split(';'))
+                    {
+                        var parts = pair.Split(new[] {'='}, 2);
+                        if (parts.Length == 2)
+                            doc[parts[0]] = parts[1];
+                    }
+                }
+            }
+
+            return doc;
+        }
+
         private string[] ReadFirstHeaders(Stream body)
         {
-            string[] fields = null;
-
+            // this is just to make sure we read the fields. If we start in the middle, we will miss the fields
             body.Seek(0, SeekOrigin.Begin);
             using (var headerReader = new StreamReader(body, Encoding.ASCII, true, 1024 * 5, true))
             {
@@ -121,12 +130,12 @@ namespace ConveyorBelt.Tooling.Parsing
                 {
                     if (line.StartsWith("#Fields: "))
                     {
-                        fields = BuildFields(line);
+                        return BuildFields(line);
                     }
                 }
             }
 
-            return fields;
+            return null;
         }
 
         protected virtual string[] GetEntries(string line)
--- test/ConveyorBelt.Tooling.Test/IisLogTests.cs
+++ test/ConveyorBelt.Tooling.Test/IisLogTests.cs
@@ -1,28 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BeeHive.DataStructures;
 using ConveyorBelt.Tooling.Parsing;
-using Microsoft.WindowsAzure.Storage.Table;
-using Moq;
 using Xunit;
 
 namespace ConveyorBelt.Tooling.Test
 {
     public class IisLogTests
     {
+        private const string SeventhRowKey = "u_ex15020701_1987";
+
         [Fact]
         public void TestDataFile_ExtractsAllRecords()
         {
             var stream = new MemoryStream(File.ReadAllBytes("IisLog.txt"));
             var parser = new IisLogParser();
             var entities = parser.Parse(stream, new Uri("http://shipish/e277461e28dd4309af674f083094c568/Test.Presentation.Web.Api/Test.Presentation.Web.Api_IN_0/Web/W3SVC1273337584/u_ex15020701.log", UriKind.Absolute), new DiagnosticsSourceSummary()).ToArray();
-            Assert.Equal(entities[0]["s-computername"], "RD00155D4A0E2E");
-            Assert.Equal(entities[0]["cs-method"], "GET");
-            Assert.Equal(entities[1]["cs-uri-stem"], "/product/catalogue/v2/productgroups/ctl/4650127");
+            Assert.Equal("RD00155D4A0E2E", entities[0]["s-computername"]);
+            Assert.Equal("GET", entities[0]["cs-method"]);
+            Assert.Equal("store=AU", entities[0]["cs-uri-query"]);
+            Assert.Equal("/product/catalogue/v2/productgroups/ctl/4650127", entities[1]["cs-uri-stem"]);
+            Assert.Equal(SeventhRowKey, entities[6]["RowKey"]);
+        }
+
+        [Fact]
+        public void TestDataFile_ExtractsAllRecordsInOffsetRange()
+        {
+            var stream = new MemoryStream(File.ReadAllBytes("IisLog.txt"));
+            var parser = new IisLogParser();
+            var entities = parser.Parse(
+                stream,
+                new Uri("http://shipish/e277461e28dd4309af674f083094c568/Test.Presentation.Web.Api/Test.Presentation.Web.Api_IN_0/Web/W3SVC1273337584/u_ex15020701.log", UriKind.Absolute),
+                new DiagnosticsSourceSummary(),
+                504/*6th row*/, 691/*7th row*/).ToArray();
+
+            Assert.Equal("RD00155D4A0E2E", entities[0]["s-computername"]);
+            Assert.Equal("GET", entities[0]["cs-method"]);
+            Assert.Equal("store=US", entities[0]["cs-uri-query"]);
+        }
+
+        [Fact]
+        public void TestDataFile_ExtractsAllRecordsStartingFromOffsetWithChangingHeaders()
+        {
+            var stream = new MemoryStream(File.ReadAllBytes("IisLog.txt"));
+            var parser = new IisLogParser();
+            var entities = parser.Parse(
+                stream,
+                new Uri("http://shipish/e277461e28dd4309af674f083094c568/Test.Presentation.Web.Api/Test.Presentation.Web.Api_IN_0/Web/W3SVC1273337584/u_ex15020701.log", UriKind.Absolute),
+                new DiagnosticsSourceSummary(),
+                1792/*15th row*/).ToArray();
+
+            Assert.Equal("RD00155D4A0E2E", entities[0]["s-computername"]);
+            Assert.Equal("GET", entities[0]["cs-method"]);
+            Assert.Equal("store=US", entities[0]["cs-uri-query"]);
+            Assert.Equal("/product/catalogue/v2/productgroups/ctl/5206173", entities[0]["cs-uri-stem"]);
+            Assert.Equal("78", entities[0]["time-taken"]);
+            Assert.Equal(SeventhRowKey, entities[0]["RowKey"]);
         }
     }
 }
